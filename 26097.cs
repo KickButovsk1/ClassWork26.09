300855366a0b81e7b3f01be7e4d9f6165d1aeee1
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-26 09:30:07 +0500

    Add files via upload

---
+++ 26097.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine();
+            //GetSymbols(str);
+            //Ex2(str);
+            //Ex3(str);
+            //Ex4(str);
+            //int collichestvo = Convert.ToInt32(Console.ReadLine());
+            // Ex5(str, collichestvo);
+            // Ex6(str);
+            Ex7(str);
+            Console.ReadKey();
+
+        }
+        private static void Ex7(string str)
+        {
+            StringBuilder str2 = new StringBuilder();
+            int l = str.Length;
+            str2.Insert(0, "*", l);
+            str2.Insert(str.Length, str);
+            int h=str2.Length;
+            str2.Insert(str2.Length, "*", l);
+            
+           
+            
+            Console.WriteLine(str2);
+            
+        }
+            private static void Ex6(string str)
+        {
+            StringBuilder str2= new StringBuilder();
+            string plusi = "++++";
+            string minusi = "-----";
+            str2.Insert(0, str);
+            str2.Insert(0, plusi);
+            str2.Insert(str2.Length, minusi);
+            Console.WriteLine(str2);
+        }
+        private static void Ex5(string str, int collichestvo)
+        {
+            
+            for (int i = 0; i < collichestvo; i++)
+            {
+                Console.WriteLine(str);
+            }
+        }
+        private static void Ex4(string str)
+        {
+            StringBuilder t = new StringBuilder();
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                t.Append(str[i]);
+            }
+            Console.WriteLine(t);
+        }
+            private static void Ex3(string str)
+        {
+            StringBuilder str2 = new StringBuilder();
+            for (int i = 0; i < str.Length; i += 2)
+            {
+                str2.Append(str[i]);
+            }
+            Console.WriteLine(str2);
+        }
+
+        private static void Ex2(string str)
+        {
+            StringBuilder str2= new StringBuilder();
+            for (int i = str.Length - 1; i >= 0; i--) 
+            {
+                str2.Append(str[i]);
+            }
+                Console.Write(str2);
+        }
+
+        /// <summary>
+        /// Печать символа столбиком
+        /// </summary>
+        /// <param name="str"></param>
+        private static void GetSymbols(string str)
+        {
+            foreach (char item in str)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+    }
+}
